bb4b8413a2e7bb79926c7838f309f4d6aa682f7a
kangrae <kangrae0501@gmail.com>
2024-11-21 16:17:54 +0900

    feat: 예외 사항과 페이지 계산 기능 구현
    Co-authored-by: g0rnn <gyunh01@pukyong.ac.kr>

--- src/main/java/com/boomball/Main.java
+++ src/main/java/com/boomball/Main.java
@@ -1,8 +1,43 @@
 package com.boomball;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public static int solution(List<Integer> pobi, List<Integer> crong) {
+        // validate
+        if (pobi.size() != 2 || crong.size() != 2) {
+            return -1;
+        }
+        if (!isSequential(pobi) || !isSequential(crong)) {
+            return -1;
+        }
+        if (pobi.get(0) % 2 != 0 || crong.get(0) % 2 != 0) {
+            return -1;
+        }
+
+        // calculate
+
+        return 0;
+    }
+
+    private static int calculate(Integer pageNumber) {
+        int sum = 0;
+        int mul = 1;
+
+        while (pageNumber != 0) {
+            sum += pageNumber % 10;
+            mul *= pageNumber % 10;
+            pageNumber /= 10;
+        }
+        return Math.max(sum, mul);
+    }
+
+    private static boolean isSequential(List<Integer> pages) {
+        return pages.get(0) == pages.get(1) - 1;
     }
 }
 
